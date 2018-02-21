3ba6749b892cd1a077cbd10506174880574fc937
Jason.Nazario <c-jason.nazario@milliman.com>
2018-02-21 11:10:09 -0800

    added typescript and webpack

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+// The @Component decorator indicates the class is a Vue component
+@Component({
+    // All component options are allowed in here
+    template: '<button @click="onClick">Click!</button>'
+})
+export default class MyComponent extends Vue {
+    // Initial data can be declared as instance properties
+    message : string = 'Hello!'
+
+    // Component methods can be declared as instance methods
+    onClick() : void {
+        window.alert(this.message)
+    }
+}
+
+declare module 'vue/types/vue' {
+    // 3. Declare augmentation for Vue
+    interface Vue {
+        $myProperty : string
+    }
+}
+
+var vm = new Vue()
+console.log(vm.$myProperty) // This should compile successfully
