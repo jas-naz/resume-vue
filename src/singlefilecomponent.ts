971a730cda9ab445c76baf2e2c9067002e93e010
jas-naz <j-naz@nazd.us>
2018-02-25 19:45:00 -0800

    fadeout added, education area

--- src/singlefilecomponent.ts
+++ src/singlefilecomponent.ts
@@ -9,45 +9,70 @@ export default {
     },
     template : `
         <div v-if='resumeData != null'>
-            <h1>Resume Component</h1>
             <div class='area'>
                 <side-bar typeclass='profile' propa='My' propb='Profile'></side-bar>
-                <div class='grouped'>
-                    <h2>Jason Nazario</h2>
-                    <p>{{ resumeData.introduction }}</p>
-                    <div v-for='item in resumeData'>
-                        <!-- <div v-for='i in item'>
-                        //     {{ i.name }}
-                        //     {{ i.title }}
-                        // </div> -->
+                <div class='grouped profile'>
+                    <div class='name'>{{ resumeData.profile.first }}
+                        <div>{{ resumeData.profile.last }}</div>
                     </div>
+                    <p>{{ resumeData.introduction }}</p>
+                    <div class='col col-1'>
+                        <div>Adress: {{ resumeData.profile.address }}</div>
+                        <div></div>
+                    </div>
+                    <div class='col col-1'>
+                        <div>Mobile: {{ resumeData.profile.mobile }}</div>
+                        <div>Skype: {{ resumeData.profile.skype }}</div>
+                    </div>
+                    <div class='col col-1'>
+                        <div>{{ resumeData.profile.email }}</div>
+                        <div>{{ resumeData.profile.url }}</div>
+                    </div>
+                    <div class="fadeout"></div>
                 </div>
             </div>
+            <!-- EXPERIENCE -->
             <div class='area'>
                 <side-bar typeclass='experience' propa='My' propb='Experience'></side-bar>
-                <div class='grouped'>
-                    <h2>Experience</h2>
-                    <div v-for='work in resumeData.experience'>
-                        {{ work.title }} {{ work.company }}, {{ work.location }}
+                <div class='grouped separator'>
+                    <div class="small-label experience">My Experience</div>
+                    <div v-for='work in resumeData.experience' class='item'>
+                        <div class='col col-1'>
+                            <h2>{{ work.title }}</h2>
+                            <div>{{ work.dates }}</div>
+                        </div>
+                        <div class='col col-2'>
+                            <h2>{{ work.company }}, {{ work.location }}</h2>
+                            <div>{{ work.details }}</div>
+                        </div>
                     </div>
+                    <div class="fadeout"></div>
                 </div>
             </div>
+            <!-- EDUCATION -->
             <div class='area'>
                 <side-bar typeclass='education' propa='My' propb='Education'></side-bar>
-                <div class='grouped'>
-                    <h2>Education</h2>
-                    <div v-for='school in resumeData.education' class='section'>
-                        <div>{{ school.name }}</div>
-                        <div>{{ school.level }}</div>
-                        <div>{{ school.level }}</div>
+                <div class='grouped separator'>
+                    <div class="small-label education">My Education</div>
+                    <div v-for='school in resumeData.education' class='item'>
+                        <div class='col col-1'>
+                            <h2>{{ school.degree }}&nbsp;</h2>
+                            <div>{{ school.dates }} </div>
+                        </div>
+                        <div class='col col-2'>
+                            <h2>{{ school.name }}</h2>
+                            <div>{{ school.details }}</div>
+                        </div>
                     </div>
+                    <div class="fadeout"></div>
                 </div>
             </div>
             <div class='area'>
                 <side-bar typeclass='skills' propa='Skills &' propb='Interests'></side-bar>
-                <div class='grouped'>
+                <div class='grouped separator'>
+                    <div class="small-label skills">Skills & Interests</div>
                     <h2>Skills</h2>
-                    <div v-for='skill in resumeData.skills' class='section skill'>
+                    <div v-for='skill in resumeData.skills' class='item skill'>
                         <div>{{ skill.name }}</div>
                         <div>{{ skill.level }}</div>
                         <div>{{ skill.level }}</div>
